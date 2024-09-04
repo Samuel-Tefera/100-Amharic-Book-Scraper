f32e9d760670cab4540ab1ec1ff9081cabffd2c6
Samuel-Tefera <samuelteferab@gmail.com>
2024-09-04 18:37:49 +0300

    scrape 113 additional books

--- books.py
+++ books.py
@@ -1,7 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
 
-books_url = 'https://www.goodreads.com/list/show/89548.Best_Amharic_Books'
+# URL for first page
+books_url1 = 'https://www.goodreads.com/list/show/89548.Best_Amharic_Books'
+# URL for second page
+books_url2 = 'https://www.goodreads.com/list/show/89548.Best_Amharic_Books?page=2'
+# URL for third page
+books_url3 = 'https://www.goodreads.com/list/show/89548.Best_Amharic_Books?page=3'
+
+books_urls = [books_url1, books_url2, books_url3]
+
 
 headers = {
        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
@@ -10,16 +18,18 @@ headers = {
 def books_detail_url(book_link):
     return f'https://www.goodreads.com{book_link}'
 
-book_res = requests.get(url=books_url, headers=headers)
-web_page = book_res.text
-soup = BeautifulSoup(web_page, 'html.parser')
-
 books_title = []
 books_link = []
-for title in soup.find_all('a', {'class' : 'bookTitle'}):
-    books_title.append(title.find('span', recursive=False).get_text())
-    books_link.append(books_detail_url(title['href']))
 
-books_author = []
-for author in soup.find_all('a', {'class' : 'authorName'}):
-    books_author.append(author.find('span', recursive=False).get_text())
+for books_url in books_urls:
+    book_res = requests.get(url=books_url, headers=headers)
+    web_page = book_res.text
+    soup = BeautifulSoup(web_page, 'html.parser')
+
+    for title in soup.find_all('a', {'class' : 'bookTitle'}):
+        books_title.append(title.find('span', recursive=False).get_text())
+        books_link.append(books_detail_url(title['href']))
+
+    books_author = []
+    for author in soup.find_all('a', {'class' : 'authorName'}):
+        books_author.append(author.find('span', recursive=False).get_text())
--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import os
 
 import books, book_description
 
+json_file_path = 'data/amharic_books.json'
+os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
+
 data = {}
 
 for i in range(len(books.books_title)):
-    data[i+1] = {
+    data[i] = {
         'title' : books.books_title[i],
         'author' : books.books_author[i],
         'cover_page' : book_description.books_cover[i],
         'rating' : book_description.books_rating[i],
     }
-
-json_file_path = 'data/amharic_books.json'
-
-os.makedirs(os.path.dirname(json_file_path), exist_ok=True)
 
 with open(json_file_path, 'w', encoding='utf-8') as json_file:
     json.dump(data, json_file, ensure_ascii=False, indent=4)
